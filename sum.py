45280c4227f00d947766f3e15a062547d265e9f9
chu23465 <130033130+chu23465@users.noreply.github.com>
2026-04-26 14:48:55 +0530

    Init

---
+++ sum.py
@@ -0,0 +1,11 @@
+﻿class add():
+    def __init__(self, a: int, b: int):
+        self.a = a
+        self.b = b
+    def sum(self):
+        return (a + b)
+    
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+x = add(a, b)
+print(x.sum())
